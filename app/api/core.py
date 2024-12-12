aae5ba976ef465c5d3a38a64eb67debaf0e54a87
Aakash <aakash19here@gmail.com>
2024-12-12 19:41:29 +0530

    feat: add document intelligence

--- app/api/core.py
+++ app/api/core.py
@@ -1,8 +1,124 @@
 from fastapi import APIRouter, HTTPException, Path
+import os
+import json
+import base64
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.documentintelligence import DocumentIntelligenceClient
+from azure.ai.documentintelligence.models import AnalyzeResult
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+from azure.ai.documentintelligence.models import ContentFormat
+import pathlib
+from dotenv import load_dotenv
+
 
 router = APIRouter()
+load_dotenv()
 
 
-@router.get("/")
+current_file = pathlib.Path(__file__).parent.parent.parent
+
+# workaround for now
+pdf_path = current_file / "app/api/report.pdf"
+
+
+# set `<your-endpoint>` and `<your-key>` variables with the values from the Azure portal
+key = os.getenv("AZURE_KEY")
+endpoint = os.getenv("AZURE_ENDPOINT")
+
+
+def get_words(page, line):
+    result = []
+    for word in page.words:
+        if _in_span(word, line.spans):
+            result.append(word)
+    return result
+
+
+def _in_span(word, spans):
+    for span in spans:
+        if word.span.offset >= span.offset and (
+            word.span.offset + word.span.length
+        ) <= (span.offset + span.length):
+            return True
+    return False
+
+
+def analyze_layout():
+    # sample document
+    # formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf"
+    # file_bytes = open('/content/Al.Noor_GPAReports (2) (1).pdf', 'rb')
+
+    document_intelligence_client = DocumentIntelligenceClient(
+        endpoint=endpoint, credential=AzureKeyCredential(key)
+    )
+
+    # TODO: fetch content from a signed url
+    poller = document_intelligence_client.begin_analyze_document(
+        "prebuilt-layout", analyze_request={'base64Source': base64.b64encode(open(pdf_path, 'rb').read()).decode("utf-8")},
+    )
+
+    result: AnalyzeResult = poller.result()
+
+    if result.styles and any([style.is_handwritten for style in result.styles]):
+        print("Document contains handwritten content")
+    else:
+        print("Document does not contain handwritten content")
+
+    for page in result.pages:
+        print(f"----Analyzing layout from page #{page.page_number}----")
+        print(
+            f"Page has width: {page.width} and height: {page.height}, measured with unit: {page.unit}"
+        )
+
+        if page.lines:
+            for line_idx, line in enumerate(page.lines):
+                words = get_words(page, line)
+                print(
+                    f"...Line # {line_idx} has word count {len(words)} and text '{line.content}' "
+                    f"within bounding polygon '{line.polygon}'"
+                )
+
+                for word in words:
+                    print(
+                        f"......Word '{word.content}' has a confidence of {word.confidence}"
+                    )
+
+        if page.selection_marks:
+            for selection_mark in page.selection_marks:
+                print(
+                    f"Selection mark is '{selection_mark.state}' within bounding polygon "
+                    f"'{selection_mark.polygon}' and has a confidence of {selection_mark.confidence}"
+                )
+
+    if result.tables:
+        for table_idx, table in enumerate(result.tables):
+            print(
+                f"Table # {table_idx} has {table.row_count} rows and "
+                f"{table.column_count} columns"
+            )
+            if table.bounding_regions:
+                for region in table.bounding_regions:
+                    print(
+                        f"Table # {table_idx} location on page: {region.page_number} is {region.polygon}"
+                    )
+            for cell in table.cells:
+                print(
+                    f"...Cell[{cell.row_index}][{cell.column_index}] has text '{cell.content}'"
+                )
+                if cell.bounding_regions:
+                    for region in cell.bounding_regions:
+                        print(
+                            f"...content on page {region.page_number} is within bounding polygon '{region.polygon}'"
+                        )
+
+    print("----------------------------------------")
+    json_data = json.loads(json.dumps(result, default=lambda o: o.__dict__))
+    return json_data
+
+
+@router.post("/")
 async def evaluate():
-    return {"status": "success"}
+
+    result = analyze_layout()
+
+    return {"response": result}
